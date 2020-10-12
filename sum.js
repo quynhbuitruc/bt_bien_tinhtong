2dfdd4f7431816c1654ad1522a3fbe828125de5a
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-12 16:20:12 +0900

    chèn text vào hienthi

--- sum.js
+++ sum.js
@@ -1,6 +1,6 @@
-function myFunction1 () {
+function tinhTong() {
     let so1 = Number(document.getElementById("soThuNhat").value);
     let so2 = Number(document.getElementById("soThuNhi").value);
     let sum = so1 + so2;
-    
+    document.getElementById("hienthi").innerText = sum;;
 }
