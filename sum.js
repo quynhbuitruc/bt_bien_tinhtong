a89ad6c3f91d9e16529d5ebd16a424b6ab3b41a7
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-12 16:06:42 +0900

    đặt biến so1 va som2 và su

---
+++ sum.js
@@ -0,0 +1,6 @@
+function myFunction1 () {
+    let so1 = Number(document.getElementById("soThuNhat").value);
+    let so2 = Number(document.getElementById("soThuNhi").value);
+    let sum = so1 + so2;
+    
+}
